cde6071249fee21868f0df4fde625b4295db5a79
Tyler Parrow <tpparrow@gmail.com>
2024-03-24 16:41:51 -0500

    Added FileManager class and Character class

---
+++ FileManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace DnD_Character_Sheet
+{
+    internal class FileManager
+    {
+        //FileName
+        private string fileName;
+        
+        public FileManager() { }
+        public FileManager(string fileName) { }
+        
+        public string GetFileName(string fileName)
+        {
+            return fileName;
+        }
+
+        public Character GetCharacter() {  return null; }
+    }
+}
